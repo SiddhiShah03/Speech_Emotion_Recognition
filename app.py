78cd737bc10bcbc498899ca99db3d3c0395ebb2a
SiddhiShah03 <109909860+SiddhiShah03@users.noreply.github.com>
2025-04-08 13:06:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,3 +50,31 @@ if uploaded_file is not None:
 
         except Exception as e:
             st.error("⚠️ Error during prediction: " + str(e))
+
+
+# Inject custom CSS for background
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url("emoji.png");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+    .stApp::before {{
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(255, 255, 255, 0.7); /* adjust transparency */
+        z-index: -1;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
