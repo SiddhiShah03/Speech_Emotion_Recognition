5ac65de7bb943ef4212b4d3acb5a477088822f34
SiddhiShah03 <109909860+SiddhiShah03@users.noreply.github.com>
2025-04-08 13:11:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ st.markdown(
         left: 0;
         width: 100%;
         height: 100%;
-        background-color: rgba(255, 255, 255, 0.7);  /* adjust for transparency */
+        background-color: rgba(255, 255, 255, 0.85);  /* adjust for transparency */
         z-index: -1;
     }}
     </style>
